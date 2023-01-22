af8b5427904a60ce72350abc06df36e9f53f90fb
thuranaing <65816005+thuranaing10@users.noreply.github.com>
2023-01-22 10:22:03 +0630

    Add API

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
-dotenv.config({ path: 'config.env' });
+
+dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
-// log request
+// log requests
 app.use(morgan('tiny'));
 
+// mongodb connection
+connectDB();
+
 // parse request to body-parser
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
-app.set("view engine", "ejs");
-// app.set("views", path.resolve(__dirname, "views/ejs"));
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
-app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
-app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// load routers
+app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, function () {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
